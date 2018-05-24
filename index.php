9a9870f3c7b5ebac6ccc3823d10dd2fd2b368264
PierreHermey <pierre.hermey@laposte.net>
2018-05-24 09:11:44 +0200

    MVC pattern

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require 'controlers/movie.php';
+
+// routeur
+switch($_GET['page']) {
+
+     case 'home':
+          indexPage();
+          break;
+     
+     case 'detail':
+          detailPage($_GET['id']);
+          break;
+
+//     case 'detail':
+//          detailPage();
+//          break;
+//
+//     default:
+//          header('HTTP/1.0 404 Not Found');
+//          require './views/404.php';
+//          break;
+}
+?>
